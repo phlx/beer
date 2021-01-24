5e721c8e5395ff410557f630ab0582b89e5e79ac
Stanislav Varnavsky <phlx@ya.ru>
2021-01-25 02:43:08 +0300

    Fixes

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = 80;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
